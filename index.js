daf19382c4a7d7e096900f3bff985c507e0c952b
Arafat-yousuf <arafatapurba1997@gmail.com>
2020-07-26 23:16:43 +0600

    security added

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const id = "dbDarksoul";
-const pass = "6WpwIgaQnOCtmM2r";
-
 //database conn
 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://dbDarksoul:6WpwIgaQnOCtmM2r@cluster0.xga2q.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const uri = process.env.DB_PATH;
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("onlineStore").collection("products");
